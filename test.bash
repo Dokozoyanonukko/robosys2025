a0b3d2d2ac2f7e9b7fdbe241c46e96ffc4451440
Sippo <s24C1098HS@s.chibakoudai.jp>
2025-11-30 23:12:00 +0900

    mod test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2025 Dokozoyanonukko 
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,19 +9,70 @@ ng () {
 
 res=0
 
-### NOMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+### NORMAL INPUT ###
+
+input_normal="2025/11/29 23:30
+2025/11/30 07:00
+身支度 30
+朝食 20
+休憩 10"
+
+expected="2025-11-30 06:00:00
+2025-11-30 06:55:00
+60
+390"
+
+out=$(echo "$input_normal" | ./schedule)
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
+# 1: テキストでない（＝ただの文字列）
+out=$(echo あ | ./schedule)
 [ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
+# 2: 空入力
+out=$(echo "" | ./schedule)
 [ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
 
+# 3: 3行未満
+out=$(echo "2025/11/29 23:30" | ./schedule)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+# 4: 日付と時刻の間にスペースがない
+out=$(echo "2025/11/29_23:30
+2025/11/30 07:00
+身支度 30" | ./schedule)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+# 5: タスクと所要時間の間にスペースがない
+out=$(echo "2025/11/29 23:30
+2025/11/30 07:00
+朝食20" | ./schedule)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+# 6: 時刻のコロンや数字が全角になっている
+out=$(echo "2025/11/29 ２３：３０
+2025/11/30 07:00
+身支度 30" | ./schedule)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+# 7: 日付の区切りが "-" になっている
+out=$(echo "2025-11-29 23:30
+2025/11/30 07:00
+身支度 30" | ./schedule)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+
+###結果
+#
+####結果は###
 [ "${res}" = 0 ] && echo OK
 exit $res
 
